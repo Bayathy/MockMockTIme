cd4a36596e38e2b43e81eef8eda295237bda8aea
Bayathy <misosoup0321@icloud.com>
2023-02-11 17:47:28 +0900

    feat: create sections file

--- src/components/Section/main-section.tsx
+++ src/components/Section/main-section.tsx
@@ -1,9 +1,14 @@
-import type { FC } from "react"
+import type { FC } from "react";
 
-export const MainSection : FC = () => {
-    return(
-        <section>
-            <p>main section</p>
-        </section>
-    )
-}
+import tw from "twin.macro";
+
+import { Card } from "../Card";
+
+export const MainSection: FC = () => {
+  return (
+    <section css={tw`m-auto max-w-2xl`}>
+      <h2 css={tw`text-2xl`}>main section</h2>
+      <Card />
+    </section>
+  );
+};
