4588e21208fa2318ffb2d5a7102c0d904f65991a
Bayathy <misosoup0321@icloud.com>
2023-02-11 17:57:21 +0900

    feat: create avater file

--- src/components/commons/avatar.tsx
+++ src/components/commons/avatar.tsx
@@ -1,14 +1,14 @@
-import type {FC} from "react";
+import type { FC } from "react";
 
 import Image from "next/image";
 import tw from "twin.macro";
 
-export type AvaterProparty = {
-    src : string
-}
+export type AvatarProperty = {
+  src: string;
+};
 
-export const Avater : FC<AvaterProparty> = ({src}) => (
-    <div css={tw`relative h-8 w-8 overflow-hidden rounded-full `}>
-        <Image fill css={tw`aspect-square object-cover`} src={src} alt={"test"}/>
-    </div>
-)
+export const Avatar: FC<AvatarProperty> = ({ src }) => (
+  <div css={tw`relative h-8 w-8 overflow-hidden rounded-full `}>
+    <Image fill css={tw`aspect-square object-cover`} src={src} alt={"test"} />
+  </div>
+);
