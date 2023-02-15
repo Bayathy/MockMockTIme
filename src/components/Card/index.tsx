bcf9faa8851bc2f5f076e6b767f0cb9473658672
Bayathy <misosoup0321@icloud.com>
2023-02-16 01:38:31 +0900

    feat: change Card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -5,12 +5,19 @@ import tw from "twin.macro";
 
 import { Avatar } from "../commons/avatar";
 
-export const Card: FC = () => {
+export type CardProperty = {
+  title: string;
+  guestImage: Array<string>;
+};
+
+export const Card: FC<CardProperty> = ({ title, guestImage }) => {
   return (
     <Link href={"/"} css={tw`block`}>
-      <div css={tw`w-1/3 rounded-xl bg-slate-50 p-4`}>
-        <h3 css={tw`text-sm`}>React勉強中</h3>
-        <Avatar src={"/test.png"} />
+      <div css={tw`rounded-xl bg-slate-50 p-4`}>
+        <h3 css={tw`text-sm`}>{title}</h3>
+        {guestImage.map((index, key) => (
+          <Avatar key={key} src={index} />
+        ))}
       </div>
     </Link>
   );
