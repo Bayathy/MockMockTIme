686495cb0512032539ea91b975e18b135110f1d5
Bayathy <misosoup0321@icloud.com>
2023-02-16 01:38:46 +0900

    feat: create Mock API Routes

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,12 +1,17 @@
 import { rest } from "msw";
 
+import type { CardProperty } from "../components/Card";
+
 export const handlers = [
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   rest.get("/list", (req, res, ctx) => {
     return res(
-      ctx.json({
-        firstName: "John",
-      })
+      ctx.json<Array<CardProperty>>([
+        {
+          title: "test1",
+          guestImage: ["/test.png"],
+        },
+      ])
     );
   }),
 ];
